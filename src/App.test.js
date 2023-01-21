144e8f099f046de2ba7a546679e7408b7f2be70f
Melik Melikyan <mhmelikyan@gmail.com>
2023-01-21 00:23:45 -0500

    Testing App.test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import renderer from 'react-test-renderer'
+test('render a snapshot', () => {
+  const tree = renderer.create(<App/>).toJSON()
+  expect (tree).toMatchSnapshot()
+  
+})
